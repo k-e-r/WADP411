65fa386fd234714e9faa9b261b1fa723c4f89bbd
k-e-r <damajonboua@gmail.com>
2022-01-25 09:36:24 -0800

    activate recommended responses

--- 0124_homework/script.js
+++ 0124_homework/script.js
@@ -5,7 +5,7 @@ Exercise 01
 -----------
 Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
 */
-// let p = document.querySelector('p');
+let p = document.querySelector('p');
 // p.innerHTML = p.innerHTML
 //   .split(' ')
 //   .map((val) => {
@@ -17,43 +17,43 @@ Highlight all of the words over 8 characters long in the paragraph text (with a 
 //   .join(' ');
 
 // # additional1
-const check = (val, ch) => {
-  val = val.replace(ch, '');
-  if (val.length > 8) {
-    return '<span style="background-color: yellow">' + val + '</span>' + ch;
-  }
-  return val + ch;
-};
+// const check = (val, ch) => {
+//   val = val.replace(ch, '');
+//   if (val.length > 8) {
+//     return '<span style="background-color: yellow">' + val + '</span>' + ch;
+//   }
+//   return val + ch;
+// };
 
-let p = document.querySelector('p');
-p.innerHTML = p.innerHTML
-  .replaceAll('\n', ' ')
-  .split(' ')
-  .map((val) => {
-    if (val.indexOf('.') > -1) {
-      return check(val, '.');
-    } else if (val.indexOf(',') > -1) {
-      return check(val, ',');
-    } else if (val.indexOf('!') > -1) {
-      return check(val, '!');
-    } else if (val.indexOf('?') > -1) {
-      return check(val, '?');
-    } else if (val.length > 8) {
-      return '<span style="background-color: yellow">' + val + '</span>';
-    }
-    return val;
-  })
-  .join(' ');
-
-// # additional2 of answer 1
-// p.innerHTML = p.innerText
+// let p = document.querySelector('p');
+// p.innerHTML = p.innerHTML
+//   .replaceAll('\n', ' ')
 //   .split(' ')
-//   .map(word => {
-//     return word.length > 8
-//       ? `<span style="background-color: yellow">${word}</span>`
-//       : word;
+//   .map((val) => {
+//     if (val.indexOf('.') > -1) {
+//       return check(val, '.');
+//     } else if (val.indexOf(',') > -1) {
+//       return check(val, ',');
+//     } else if (val.indexOf('!') > -1) {
+//       return check(val, '!');
+//     } else if (val.indexOf('?') > -1) {
+//       return check(val, '?');
+//     } else if (val.length > 8) {
+//       return '<span style="background-color: yellow">' + val + '</span>';
+//     }
+//     return val;
 //   })
 //   .join(' ');
+
+// # additional2 of answer 1
+p.innerHTML = p.innerText
+  .split(' ')
+  .map((word) => {
+    return word.length > 8
+      ? `<span style="background-color: yellow">${word}</span>`
+      : word;
+  })
+  .join(' ');
 
 /*
   Exercise 02
@@ -66,8 +66,8 @@ a.href = 'http://officeipsum.com/';
 /*
  * Either is fine
  */
-// a.innerText = 'link';
-a.textContent = 'link';
+a.innerText = 'link';
+// a.textContent = 'link';
 p.after(a);
 
 /*
@@ -76,7 +76,8 @@ p.after(a);
   Split each new sentence on to a separate line in the paragraph text.
   A sentence can be assumed to be a string of text terminated with a period (.)
 */
-p.innerHTML = p.innerHTML.split('.').join('.<br/>');
+// p.innerHTML = p.innerHTML.split('.').join('.<br/>');
+p.innerHTML = p.innerHTML.split('.').join('.</p><p>') + '</p>';
 
 /*
   Exercise 04
@@ -84,9 +85,9 @@ p.innerHTML = p.innerHTML.split('.').join('.<br/>');
   Count the number of words in the paragraph tag and display the count afer the heading.
   You can assume that all words are separated by one singular whitespace.
 */
-const div = document.createElement('div');
-div.innerText = p.innerText.split(' ').length + ' words';
-document.querySelector('h1').after(div);
+// const div = document.createElement('div');
+// div.innerText = p.innerText.split(' ').length + ' words';
+// document.querySelector('h1').after(div);
 
 // # additional2
 // const div = document.createElement('div');
@@ -100,6 +101,7 @@ document.querySelector('h1').after(div);
 // // for last </br>
 // div.innerText = p.innerText.split(/ |\n/).length - 1 + ' words';
 // document.querySelector('h1').after(div);
+console.log(p.textContent.trim().split(' ').length);
 
 /*
   Exercise 05
