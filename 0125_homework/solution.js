63a12fbfad3b026ee925b5cce088f737d4d02310
k-e-r <damajonboua@gmail.com>
2022-01-25 13:35:48 -0800

    add another answer

--- 0125_homework/solution.js
+++ 0125_homework/solution.js
@@ -6,14 +6,25 @@ btn.addEventListener('click', (e) => {
   e.preventDefault();
   if (!input.value) return;
 
-  const newItemText = input.value;
-  const newLI = document.createElement('li');
-  newLI.innerHTML = `
-    <p>${newItemText}</p>
-    <p>
-      <i class="fa fa-pencil"></i>
-      <i class="fa fa-times"></i>
-    </p>`;
-  itemsUL.appendChild(newLI);
+  /**
+   * answer 2
+   */
+  const data = itemsUL.firstElementChild.cloneNode(true);
+  data.firstElementChild.textContent = input.value;
+  itemsUL.appendChild(data);
   input.value = '';
+
+  /**
+   * answer 1
+   */
+  // const newItemText = input.value;
+  // const newLI = document.createElement('li');
+  // newLI.innerHTML = `
+  //   <p>${newItemText}</p>
+  //   <p>
+  //     <i class="fa fa-pencil"></i>
+  //     <i class="fa fa-times"></i>
+  //   </p>`;
+  // itemsUL.appendChild(newLI);
+  // input.value = '';
 });
