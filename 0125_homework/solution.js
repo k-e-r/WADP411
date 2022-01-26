c28977a10f20f9830abce33f6b2feb415afb1390
k-e-r <damajonboua@gmail.com>
2022-01-26 02:09:52 -0800

    add brank space...

--- 0125_homework/solution.js
+++ 0125_homework/solution.js
@@ -1,18 +1,39 @@
-const btn = document.querySelector('#add-btn');
+const addBtn = document.querySelector('#add-btn');
 const input = document.querySelector('#add-input');
 const itemsUL = document.querySelector('#list');
 
-btn.addEventListener('click', (e) => {
+addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (!input.value) return;
 
   /**
-   * answer 2
+   * answer 3
    */
-  const data = itemsUL.firstElementChild.cloneNode(true);
-  data.firstElementChild.textContent = input.value;
-  itemsUL.appendChild(data);
+  const li = document.createElement('li');
+  const textP = document.createElement('p');
+  const iconP = document.createElement('p');
+  const pen = document.createElement('i');
+  const times = document.createElement('i');
+  const brank = document.createElement('span');
+
+  pen.className = 'fa fa-pencil';
+  times.className = 'fa fa-times';
+  textP.textContent = input.value;
+  li.appendChild(textP);
+  li.appendChild(iconP).appendChild(pen);
+  brank.textContent = ' ';
+  li.appendChild(iconP).appendChild(brank);
+  li.appendChild(iconP).appendChild(times);
+  itemsUL.appendChild(li);
   input.value = '';
+
+  /**
+   * answer 2 (before adding delete function)
+   */
+  // const data = itemsUL.firstElementChild.cloneNode(true);
+  // data.firstElementChild.textContent = input.value;
+  // itemsUL.appendChild(data);
+  // input.value = '';
 
   /**
    * answer 1
